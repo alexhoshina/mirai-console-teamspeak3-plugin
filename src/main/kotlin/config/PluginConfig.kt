88c2f0622bf53e79facfe7480c0f4f80da75ed5b
Hoshina <salexhoshina@users.noreply.github.com>
2024-11-25 14:59:36 +0800

    refactor(config): 将配置项从 val 改为 var 以支持修改
    - 将所有配置项的 val 关键字改为 var，使它们能够被修改
    - 这个改动允许在运行时更改配置，提高了插件的灵活性

--- src/main/kotlin/config/PluginConfig.kt
+++ src/main/kotlin/config/PluginConfig.kt
@@ -6,29 +6,29 @@ import net.mamoe.mirai.console.data.ValueDescription
 
 object PluginConfig : AutoSavePluginConfig("config") {
     @ValueDescription("TeamSpeak 服务器地址")
-    val hostName by value("localhost") // TeamSpeak 服务器地址
+    var hostName by value("localhost") // TeamSpeak 服务器地址
     @ValueDescription("TeamSpeak 服务器 Query 端口")
-    val queryPort by value(10011) // TeamSpeak 服务器 Query 端口
+    var queryPort by value(10011) // TeamSpeak 服务器 Query 端口
     @ValueDescription("TeamSpeak 服务器 Query 用户名")
-    val userName by value("serveradmin") // TeamSpeak 服务器 Query 用户名
+    var userName by value("serveradmin") // TeamSpeak 服务器 Query 用户名
     @ValueDescription("TeamSpeak 服务器 Query 密码")
-    val password by value("password") // TeamSpeak 服务器 Query 密码
+    var password by value("password") // TeamSpeak 服务器 Query 密码
     @ValueDescription("TeamSpeak 服务器虚拟服务器 ID")
-    val virtualServerId by value(1) // TeamSpeak 服务器虚拟服务器 ID
+    var virtualServerId by value(1) // TeamSpeak 服务器虚拟服务器 ID
     @ValueDescription("目标群组 ID")
-    val targetGroupIds by value(mutableListOf<Long>(0,1)) // 目标群组 ID
+    var targetGroupIds by value(mutableListOf<Long>(0,1)) // 目标群组 ID
     @ValueDescription("目标用户 ID")
-    val targetUserIds by value(mutableListOf<Long>(0,1)) // 目标用户 ID
+    var targetUserIds by value(mutableListOf<Long>(0,1)) // 目标用户 ID
     @ValueDescription("排除的 TS UID")
-    val excludedUIDs by value(mutableListOf("ServerQuery","Unknown")) // 排除的 TS UID
+    var excludedUIDs by value(mutableListOf("ServerQuery","Unknown")) // 排除的 TS UID
     @ValueDescription("频道缓存刷新间隔，单位秒")
-    val channelCacheRefreshInterval by value<Long>(600)// 频道缓存刷新间隔，单位秒
+    var channelCacheRefreshInterval by value<Long>(600)// 频道缓存刷新间隔，单位秒
     @ValueDescription("心跳间隔，单位秒")
-    val heartbeatInterval by value<Long>(60)// 心跳间隔，单位秒
+    var heartbeatInterval by value<Long>(60)// 心跳间隔，单位秒
     @ValueDescription("监听循环延迟，单位秒")
-    val listenLoopDelay by value<Long>(1)// 监听循环延迟，单位秒
+    var listenLoopDelay by value<Long>(1)// 监听循环延迟，单位秒
     @ValueDescription("默认模板")
-    val defaultTemplates by value(mapOf(
+    var defaultTemplates by value(mapOf(
             "join" to "用户 {nickname} (UID: {uid}) 加入了服务器",
             "leave" to "用户 {nickname} (UID: {uid}) 离开了服务器"))
 }
