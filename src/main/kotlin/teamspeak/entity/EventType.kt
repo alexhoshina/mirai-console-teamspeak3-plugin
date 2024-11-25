af61e254088e8b3e754f3ee132ec3ed7fe175908
Hoshina <salexhoshina@users.noreply.github.com>
2024-11-25 15:22:00 +0800

    feat(entity): 添加 EventType 枚举类

---
+++ src/main/kotlin/teamspeak/entity/EventType.kt
@@ -0,0 +1,10 @@
+package org.evaz.mirai.plugin.teamspeak.entity
+
+enum class EventType {
+    notifycliententerview,
+    notifyclientmoved,
+    notifytextmessage,
+    notifychanneledited,
+    notifychannelcreated,
+    notifychanneldeleted
+}
